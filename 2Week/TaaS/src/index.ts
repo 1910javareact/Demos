5b2aa8a3d8ec736bae83986a68973f09e11c4cfa
Abatson <abatson94@gmail.com>
2019-11-27 14:51:33 -0500

    Things to work on in TaaS if you want

--- 2Week/TaaS/src/index.ts
+++ 2Week/TaaS/src/index.ts
@@ -3,16 +3,20 @@ import { getAllTrains } from './repositories/train-dao';
 
 
 const rootApp = e()
+//Do crud endpoints for trains
+//routes
+//and locations
+//Make routers to seperate out endpoints from index.ts
+//make services so we can do any additional processing
+//adding authentication//rework models, add sessions, do login, have auth middleware
 
 rootApp.get('/trains', [async (req:Request, res:Response)=>{
     try{
         let trains = await getAllTrains();
-    
         res.json(trains);
     }catch(e){
         res.status(e.status).json(e.message);
     }
-    
 }])
 
 rootApp.listen(1002, ()=>{
