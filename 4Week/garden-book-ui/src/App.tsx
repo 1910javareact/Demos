e197934dd7562a9606000d715e21d2f01c3ddd64
Abatson <abatson94@gmail.com>
2019-12-10 14:58:30 -0500

    initial react app

---
+++ 4Week/garden-book-ui/src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {FirstComponent} from './components/first-component/first-component'
+
+//this is a function component
+const App: React.FC = () => {
+  //it needs to return some amount of jsx
+  return (
+    //only one root tag is allowed in the jsx we return
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <FirstComponent/>
+      <FirstComponent/>
+      <FirstComponent/>
+    </div>
+    
+  );
+}
+
+export default App;
